99f6596ef5ee14e20ce0520054482f0776c943b1
Vikram Nadig <vikram.nadig@energyhelpline.com>
2018-03-21 16:30:46 +0000

    inject the list of books into the booklist component

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -5,28 +5,19 @@ import './App.css'
 class BooksGrid extends Component{
     render() {
         return (<div className="bookshelf-books">
-        <ol className="books-grid">
-          <li>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                <ShelfChanger/>
-              </div>
-              <div className="book-title">To Kill a Mockingbird</div>
-              <div className="book-authors">Harper Lee</div>
-            </div>
-          </li>
-          <li>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                <ShelfChanger/>
-              </div>
-              <div className="book-title">Ender's Game</div>
-              <div className="book-authors">Orson Scott Card</div>
-            </div>
-          </li>
-        </ol>
+          <ol className="books-grid">
+            {this.props.books.map(book =>
+              <li key={book.name}>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url({{book.backgroundImage}})' }}/>
+                    <ShelfChanger />
+                  </div>
+                  <div className="book-title">{book.name}</div>
+                  <div className="book-authors">{book.author}</div>
+                </div>
+              </li>)}
+          </ol>
       </div>)
     }
 }
