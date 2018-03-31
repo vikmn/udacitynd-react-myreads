8451e89d7c4510f8d4ea49f2ae20131be4d52fbe
Vikram Nadig <vmnadig@gmail.com>
2018-03-31 13:36:04 +0100

    replace render with component attribute for route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from "react";
 import { Link, Route } from "react-router-dom";
 
-// import * as BooksAPI from './BooksAPI'
-import "./App.css";
 import BookShelf from "./BookShelf";
 import BookSearch from "./BookSearch";
+import "./App.css";
 
 class BooksApp extends Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  };
-
   render() {
     return (
       <div className="app">
-        <Route path="/search" render={() => <BookSearch />} />
+        <Route path="/search" component={BookSearch} />
         <Route
           path="/"
           exact
